48e259c1c7691bcb6f59ef76f1778c495277bae6
Bruce Cao <bruce0379@outlook.com>
2022-09-16 16:22:18 -0400

    Adds on in App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Welcome to Web Development Fall 2022
+        </p>
+        <p>
+            Hello World! This is Chenghai!
         </p>
         <a
           className="App-link"
