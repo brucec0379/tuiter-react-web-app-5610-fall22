18a27bbc2456705f604dbde0bf9efcfe9d2c67d6
Bruce Cao <bruce0379@outlook.com>
2022-10-23 03:47:35 -0400

    a6 React.js adds ons

---
+++ src/labs/a6/index.js
@@ -0,0 +1,16 @@
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput
+  from "./conditional-output";
+
+function Assignment6() {
+ return (
+   <div>
+     <h1>Assignment 6</h1>
+     <ConditionalOutput/>
+     <Styles/>
+     <Classes/>
+   </div>
+ );
+}
+export default Assignment6;
--- src/labs/index.js
+++ src/labs/index.js
@@ -1,12 +1,11 @@
-import Nav from '../nav';
-
-function Labs () {
-  return (
-    <div>
-      <Nav/>
-      <h1>Assignment 6</h1>
-    </div>
-  );
+import Assignment6 from "./a6";
+import Nav from "../nav";
+function Labs() {
+ return (
+   <div>
+     <Nav/>
+     <Assignment6/>
+   </div>
+ );
 }
-
 export default Labs;
