3fcb433dcbd277ef84376c360454b5d07ae94de1
Bruce Cao <bruce0379@outlook.com>
2022-10-23 03:57:34 -0400

    a6 React.js adds ons

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -2,11 +2,13 @@ import Classes from "./classes";
 import Styles from "./styles";
 import ConditionalOutput
   from "./conditional-output";
+import todo from "./todo";
 
 function Assignment6() {
  return (
    <div>
      <h1>Assignment 6</h1>
+     <Todo/>
      <ConditionalOutput/>
      <Styles/>
      <Classes/>
