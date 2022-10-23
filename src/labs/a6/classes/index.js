abb8ab9f8a9824de7fba281bb1f8d33f2c965377
Bruce Cao <bruce0379@outlook.com>
2022-10-23 04:42:39 -0400

    a6 React.js adds on

--- src/labs/a6/classes/index.js
+++ src/labs/a6/classes/index.js
@@ -9,6 +9,8 @@ function Classes() {
      <div className={`${dangerous ? 'wd-bg-red' : 'wd-bg-green'}
                                         wd-fg-black wd-padding-10px`}>
             Dangerous background</div>
+     <div className={`wd-bg-${color} wd-fg-black wd-padding-10px`}>
+            Dynamic Blue background</div>
      <div className="wd-bg-yellow wd-fg-black wd-padding-10px">
        Yellow background
      </div>
