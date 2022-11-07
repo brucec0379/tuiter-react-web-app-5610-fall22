5cce0fab1b30c2a7c3905800f87e0825f63b59ef
Bruce Cao <bruce0379@outlook.com>
2022-11-06 19:32:33 -0500

    a7 Redux fa22 adds on

--- src/labs/index.js
+++ src/labs/index.js
@@ -4,17 +4,15 @@ import Assignment7 from "./a7";
 import {Routes, Route} from "react-router";
 
 function Labs() {
- return (
-   <div>
-     <Nav/>
-     <Routes>
-       <Route index
-        element={<Assignment6/>}/>
-       <Route path="/a7"
-        element={<Assignment7/>}/>
-     </Routes>
-   </div>
- );
+    return(
+        <div>
+            <Nav/>
+            <Routes>
+                <Route index element={<Assignment6/>}/>
+                <Route path="a7" element={<Assignment7/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default Labs;
