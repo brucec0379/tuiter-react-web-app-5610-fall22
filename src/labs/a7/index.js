1c6a7e73a0e819e8a5104d47114e7d47d67f9d3d
Bruce Cao <bruce0379@outlook.com>
2022-11-06 19:40:49 -0500

    a7 Redux fa22 adds on

--- src/labs/a7/index.js
+++ src/labs/a7/index.js
@@ -3,10 +3,10 @@ import ReduxExamples from "./redux-examples";
 
 const Assignment7 = () => {
  return(
-   <>
+   <div>
      <h1>Assignment 7</h1>
      <ReduxExamples/>
-   </>
+   <div/>
  );
 };
 export default Assignment7;
