9976d9415be0931c1da274f20206dff0861f5fd1
Bruce Cao <bruce0379@outlook.com>
2022-11-20 13:37:09 -0500

    a8 REST fa22 adds ons-2

--- src/services/tuits-thunks.js
+++ src/services/tuits-thunks.js
@@ -1,21 +1,27 @@
 import {createAsyncThunk}
-  from "@reduxjs/toolkit"
+    from "@reduxjs/toolkit"
 import * as service
-  from "./tuits-service"
+    from "./tuits-service"
 
 export const findTuitsThunk = createAsyncThunk(
-  'tuits/findTuits', async () =>
-    await service.findTuits()
+    'tuits/findTuits', async () =>
+        await service.findTuits()
 )
 export const deleteTuitThunk = createAsyncThunk(
-  'tuits/deleteTuit',
-  async (tuitId) => {
-    await service.deleteTuit(tuitId)
-    return tuitId
-})
+    'tuits/deleteTuit',
+    async (tuitId) => {
+        await service.deleteTuit(tuitId)
+        return tuitId
+    })
+export const createTuitThunk = createAsyncThunk(
+    'tuits/createTuit',
+    async (thunkAPI) => {
+        await service.createTuit(thunkAPI)
+        return thunkAPI
+    })
 export const updateTuitThunk =
-  createAsyncThunk(
-    'tuits/updateTuit',
-    async (tuit) =>
-      await service.updateTuit(tuit)
-)
+    createAsyncThunk(
+        'tuits/updateTuit',
+        async (tuit) =>
+            await service.updateTuit(tuit)
+    )
