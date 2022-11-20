2d3d554e344656ea989708cd37be7ec632f7f656
Bruce Cao <bruce0379@outlook.com>
2022-11-20 15:55:16 -0500

    a8 REST fa22 adds ons-12

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -6,8 +6,11 @@ const TuitStats = (props) => {
     <div className="wd-icons d-flex mt-3 text-muted">
       <span><i className="bi bi-chat"></i> <span>{props.replies}</span></span>
       <span><i className="bi bi-repeat"></i> <span>{props.retuits}</span></span>
-      <span><i className={`bi bi-heart ${props.liked ? 'text-danger' : ''}`}></i> <span>{props.likes}</span></span>
-      <span><i className={`bi bi-heart ${props.disliked ? 'text-danger' : ''}`}></i> <span>{props.dislikes}</span></span>
+        <span><i className={`bi bi-heart ${props.liked ? 'text-danger' : ''}`}></i> <span>{props.likes}</span></span>
+        <span>Likes: {tuit.likes}</span><span><i onClick={() => dispatch(updateTuitThunk({...tuit,
+                likes: tuit.likes + 1
+            })} className="bi bi-heart-fill me-2 text-danger"></i>
+        </span>
       <span><i className="bi bi-share"></i></span>
     </div>
   );
