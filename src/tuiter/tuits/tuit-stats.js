8bb99bbea11b55d38683e922024d463defc19505
Bruce Cao <bruce0379@outlook.com>
2022-11-20 16:08:53 -0500

    a8 REST fa22 adds ons-15

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import {useDispatch} from "react-redux";
 import {updateTuitThunk} from "../../services/tuits-thunks";
 
 
-const TuitStats = (props) => {
-
-  return (
+const TuitStats = ({tuit}) => {
+    const dispatch = useDispatch();
+    return (
     <div className="wd-icons d-flex mt-3 text-muted">
-      <span><i className="bi bi-chat"></i> <span>{props.replies}</span></span>
-      <span><i className="bi bi-repeat"></i> <span>{props.retuits}</span></span>
-        <span><i className={`bi bi-heart-fill ${props.liked ? 'text-danger' : ''}`}></i> <span>{props.likes}</span></span>
+      <span><i className="bi bi-chat"></i> <span>{tuit.replies}</span></span>
+      <span><i className="bi bi-repeat"></i> <span>{tuit.retuits}</span></span>
+        <span><i className={`bi bi-heart-fill ${tuit.liked ? 'text-danger' : ''}`}></i> <span>{tuit.likes}</span></span>
         <span>Likes: {tuit.likes}</span><span><i className="bi bi-heart-fill me-2 text-danger" onClick={() => dispatch(updateTuitThunk({...tuit, }))}></i>
         </span>
       <span><i className="bi bi-share"></i></span>
