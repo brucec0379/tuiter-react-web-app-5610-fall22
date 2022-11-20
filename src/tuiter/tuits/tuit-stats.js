806621ba8728ed5ee74712c6c57679f5d2ff4376
Bruce Cao <bruce0379@outlook.com>
2022-11-20 15:26:43 -0500

    a8 REST fa22 adds ons-11

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -1,43 +1,16 @@
 import React from 'react';
-import {useDispatch} from "react-redux";
-import {updateTuitThunk} from "../../services/tuits-thunks";
 
-const TuitStats = ({tuit}) => {
-    const dispatch = useDispatch();
-    return(
-   <div className="row mt-3">
-               <div className="col">
-                   <i className="bi bi-chat me-2"></i>
-                   {tuit.replies}
-               </div>
+const TuitStats = (props) => {
 
-               <div className="col">
-                   <i className="bi bi-arrow-repeat me-2"></i>
-                   {tuit.retuits}
-               </div>
+  return (
+    <div className="wd-icons d-flex mt-3 text-muted">
+      <span><i className="bi bi-chat"></i> <span>{props.replies}</span></span>
+      <span><i className="bi bi-repeat"></i> <span>{props.retuits}</span></span>
+      <span><i className={`bi bi-heart ${props.liked ? 'text-danger' : ''}`}></i> <span>{props.likes}</span></span>
+      <span><i className={`bi bi-heart ${props.disliked ? 'text-danger' : ''}`}></i> <span>{props.dislikes}</span></span>
+      <span><i className="bi bi-share"></i></span>
+    </div>
+  );
+};
 
-               <div className="col">
-                   {
-                       tuit.liked &&
-                       <i className="bi bi-heart-fill me-2 text-danger"></i>
-                   }
-                   {
-                       !tuit.liked &&
-                       <i className="bi bi-heart me-2"></i>
-                   }
-               </div>
-
-               <div>
-                   Likes: {tuit.likes}
-                   <i onClick={() => dispatch(updateTuitThunk({
-                                                                  ...tuit,
-                                                                  likes: tuit.likes + 1
-                                                              }))} className="bi bi-heart-fill me-2 text-danger"></i>
-               </div>
-               <div className="col">
-                   <i className="bi bi-share"></i>
-               </div>
-           </div>
-       )
-   }
-   export default TuitStats;
+export default TuitStats;
